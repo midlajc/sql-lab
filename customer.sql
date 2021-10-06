7f4400d884be3a656489709ef49cd0114037dcd2
midlajc <mail.midlajc@gmail.com>
2021-10-06 16:14:05 +0530

    inital commit

--- customer.sql
+++ customer.sql
@@ -1,67 +1,70 @@
 
 DROP DATABASE IF EXISTS customer;
 
-#CREATE
+#Create a table customer (cust_no varchar (5), cust_name varchar (15), age number,phone varchar (10))
 
-CREATE DATABASE customer;
-USE customer;
-CREATE TABLE customer (
-    cust_no varchar(5) primary key,
-    cust_name varchar(15) not null,
-    age INT(3),
-    phone varchar(10)
-);
+    CREATE DATABASE customer;
+    USE customer;
+    CREATE TABLE customer (
+        cust_no varchar(5) primary key,
+        cust_name varchar(15) not null,
+        age INT(3),
+        phone varchar(10)
+    );
 
-DESCRIBE customer;
-select "\n\n";
-#INSERT 5 VALUES
+    DESCRIBE customer;
 
-INSERT INTO customer (cust_no, cust_name, age, phone)
-VALUES ("cs1", "akhil", 22, "3413379793"),
-    ("cs2", "amal", 21, "4396016943"),
-    ("cs3", "ansil", 20, "2345098948"),
-    ("cs4", "edwin", 20, "3231275920"),
-    ("cs5", "aslam", 20, "1234567890");
-select "\n\n";
-SELECT * FROM customer;
+#insert 5 records and display it
 
-#ADD D_DATE TO CUSTOMER
+    INSERT INTO customer (cust_no, cust_name, age, phone)
+    VALUES ("cs1", "akhil", 22, "3413379793"),
+        ("cs2", "amal", 21, "4396016943"),
+        ("cs3", "ansil", 20, "2345098948"),
+        ("cs4", "edwin", 20, "3231275920"),
+        ("cs5", "aslam", 20, "1234567890");
 
-ALTER TABLE customer add d_birth date;
-DESCRIBE customer;
+    SELECT * FROM customer;
 
-select "\n\n";
-#CREATE CUST_PHONE FROM CUSTOMER
+#add new field d_birth with date datatype
 
-CREATE TABLE cust_phone (SELECT cust_name,phone from customer);
-DESCRIBE cust_phone;
-select "\n\n";
-SELECT * FROM cust_phone;
+    ALTER TABLE customer add d_birth date;
 
-select "\n\n";
-#DROP AGE FROM CUSTOMER
+    DESCRIBE customer;
 
-ALTER TABLE customer drop age;
-DESCRIBE customer;
+#create another table cust_phone with fields cust_name and phone from customer table
 
-select "\n\n";
-#CHANGE CUSROMER.CUST_NAME TYPE TO VARCHAR(25)
+    CREATE TABLE cust_phone (SELECT cust_name,phone from customer);
 
-ALTER TABLE customer MODIFY cust_name varchar(25);
-DESCRIBE customer;
+    DESCRIBE cust_phone;
 
-select "\n\n";
-#RENAME CUSTOMER TO CUST
+    SELECT * FROM cust_phone;
 
-ALTER TABLE customer RENAME cust;
-SHOW TABLES;
+#remove the field age
 
-select "\n\n";
-#DROP TABLES
+    ALTER TABLE customer drop age;
 
-DROP TABLE cust;
-DROP TABLE cust_phone;
+    DESCRIBE customer;
 
+#change the size of the cust_name to 25
 
-select "\n\n";
-SHOW TABLES;
+    ALTER TABLE customer MODIFY cust_name varchar(25);
+    
+    DESCRIBE customer;
+
+#delete all the records from the table
+
+    DELETE FROM customer;
+
+#rename the table cutomer to cust
+
+    ALTER TABLE customer RENAME cust;
+    
+    SHOW TABLES;
+
+#drop the table
+
+    DROP TABLE cust;
+    
+    DROP TABLE cust_phone;
+
+    SHOW TABLES;
