14229e6e9c4827b61b52095a958008e2a8a0c27b
MIDLAJ C <mail.midlajc@gmail.com>
2021-10-09 12:06:20 -0700

    question 8 while loop exit case not added

---
+++ prime.sql
@@ -0,0 +1,38 @@
+DROP DATABASE IF EXISTS prime;
+
+#Create a function to check whether a given number is prime or not
+
+    CREATE DATABASE prime;
+
+    use prime;
+
+    DELIMITER /
+
+    CREATE PROCEDURE print(IN thetext TEXT)
+    BEGIN
+        SELECT thetext AS '';
+    END/
+
+    CREATE PROCEDURE prime(IN num FLOAT)
+    BEGIN
+        DECLARE I FLOAT;
+        DECLARE che FLOAT;
+        DECLARE sta VARCHAR(1);
+        SET sta="0";
+        SET I=2;
+            WHILE I<num DO
+                IF num%I=0 THEN
+                    CALL print("The Given Number Is Not a Prime");
+                    SET sta="0";
+                    EXIT;
+                END IF;
+                SET I=I+1;
+            END WHILE;
+        IF sta!=0 THEN   
+            call print("The Given Number is Prime");
+        END IF;
+    END/
+
+    DELIMITER ;       
+
+    CALL prime(4);
