d6e75c68708fc701cc4c950c2fc9c70b40fd7025
MIDLAJ C <mail.midlajc@gmail.com>
2021-10-09 06:28:18 -0700

    8 completed

--- prime.sql
+++ prime.sql
@@ -14,25 +14,22 @@ DROP DATABASE IF EXISTS prime;
     END/
 
     CREATE PROCEDURE prime(IN num FLOAT)
-    BEGIN
+    exit_label:BEGIN
         DECLARE I FLOAT;
         DECLARE che FLOAT;
         DECLARE sta VARCHAR(1);
-        SET sta="0";
         SET I=2;
         IF num<2 THEN
             CALL print("The Given Number is less Than 2");
         ELSE    
             WHILE I<num DO
                 IF num%I=0 THEN
-                    CALL print("The Given Number Is Not a Prime");
-                    SET sta="1";
+                    CALL print(CONCAT("The Given Number ",num," Is Not a Prime"));
+                    LEAVE exit_label;
                 END IF;
                 SET I=I+1;
             END WHILE;
-            IF sta=0 THEN   
-                call print("The Given Number is Prime");
-            END IF;
+            CALL print(CONCAT("The Given Number ",num," is Prime"));
         END IF;
     END/
 
